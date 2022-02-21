1f1ceee538610b773fbc6885f768d21f2f83cb76
t-i-0414 <gutlo.ol0414@gmail.com>
2022-02-21 14:52:25 +0900

    feat(components): add Color Palette

--- packages/takudev-components/src/consts/color.ts
+++ packages/takudev-components/src/consts/color.ts
@@ -1,17 +1,30 @@
-import type { ColorModeMap } from '~/@types';
+import type {
+  ColorModeMap,
+  LightColorPaletteMap,
+  DarkColorPaletteMap,
+} from '~/@types';
 
 export const colorModeMap: ColorModeMap = {
   light: 'light',
   dark: 'dark',
 } as const;
 
-export const lightColorMap = {
-  color: '#000',
-  backgroundColor: '#fff',
+export const lightColorPaletteMap: LightColorPaletteMap = {
+  baseColor: '#fafafa',
+  textColor: '#0d0d0d',
+  borderColor: '#4f4f4f',
+  sublabelColor: '#666666',
+  primaryColor: '#7cefff',
+  secondaryColor: '#b9f5b1',
+  warningColor: '#f23535',
 } as const;
 
-export const darkColorMap = {
-  ...lightColorMap,
-  color: '#fff',
-  backgroundColor: '#000',
+export const darkColorPaletteMap: DarkColorPaletteMap = {
+  ...lightColorPaletteMap,
+  baseColor: '#0d0d0d',
+  textColor: '#fcfcfc',
+  borderColor: '#dddddd',
+  sublabelColor: '#afafaf',
+  headerColor: '#666666',
+  footerColor: '#666666',
 } as const;
