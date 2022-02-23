840b8715c0aff3bfbbdf0e3bfe5cbdb202aea6f9
t-i-0414 <gutlo.ol0414@gmail.com>
2022-02-24 04:42:38 +0900

    fix(componetns): fix handleWindowVerticalHeight

--- packages/takudev-components/src/utils/handleWindowVerticalHeight.ts
+++ packages/takudev-components/src/utils/handleWindowVerticalHeight.ts
@@ -1,12 +1,14 @@
 import { throttle } from 'throttle-debounce';
 
 export const handleWindowVerticalHeight = () => {
-  const setFillHeight = () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  };
+  if (typeof window !== 'undefined') {
+    const setFillHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
 
-  window.addEventListener('resize', throttle(16, setFillHeight));
+    window.addEventListener('resize', throttle(16, setFillHeight));
 
-  setFillHeight();
+    setFillHeight();
+  }
 };
