dd2acff326204303bdf1863f1a60588e1f49bd0a
t-i-0414 <gutlo.ol0414@gmail.com>
2022-06-05 16:59:56 +0900

    feat: add _middleware for staging authenticattion

--- packages/takudev-web/pages/_middleware.ts
+++ packages/takudev-web/pages/_middleware.ts
@@ -1,21 +1,35 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization');
+import type { Stage } from '~/types';
 
-  if (basicAuth) {
-    const auth = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(auth).split(':');
+export const middleware = (req: NextRequest) => {
+  const stage: Stage = (process.env.STAGE as Stage) ?? 'preview';
+  const shouldAuntehnticate = stage === 'staging';
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
-      return NextResponse.next();
+  if (shouldAuntehnticate) {
+    const basicAuth = req.headers.get('authorization');
+
+    if (basicAuth) {
+      const auth = basicAuth.split(/\s+/g)[1];
+      const [user, password] = Buffer.from(auth, 'base64')
+        .toString()
+        .split(':');
+
+      if (
+        user === process.env.BASIC_AUTH_USER &&
+        password === process.env.BASIC_AUTH_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
     }
+
+    return new Response('Auth required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
   }
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  });
-}
+  return NextResponse.next();
+};
