fba3ae0e4d0990f4a9c6e2ffa0a26f1429591300
t-i-0414 <gutlo.ol0414@gmail.com>
2022-07-14 23:20:54 +0900

    feat: add ProfileCard

---
+++ packages/takudev-web/components/organisms/ProfileCard/ProfileCardView.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { MENU_MODAL_ROOT_ELEMENT_ID } from '~/consts';
+
+import { ProfileCard } from '.';
+
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {
+  LightThemeTemplate,
+  DarkThemeTemplate,
+} from '~/.storybook/ColorThemeTemplates';
+
+export default {
+  title: 'Organisms/ProfileCard',
+  component: ProfileCard,
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = () => <ProfileCard />;
+
+export const Light: ComponentStory<typeof ProfileCard> = Template.bind({});
+Light.decorators = [
+  (Story, context) => (
+    <LightThemeTemplate>
+      <Story {...context.args} />
+    </LightThemeTemplate>
+  ),
+];
+
+export const Dark: ComponentStory<typeof ProfileCard> = Template.bind({});
+Dark.decorators = [
+  (Story, context) => (
+    <DarkThemeTemplate>
+      <Story {...context.args} />
+    </DarkThemeTemplate>
+  ),
+];
