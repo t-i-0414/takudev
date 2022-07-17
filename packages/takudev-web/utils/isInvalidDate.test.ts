0b7fca202e4daa7053e60e0a592b451fc33013e2
t-i-0414 <gutlo.ol0414@gmail.com>
2022-07-17 22:05:11 +0900

    feat: add isInvalidDate util function

---
+++ packages/takudev-web/utils/isInvalidDate.test.ts
@@ -0,0 +1,17 @@
+import { isInvalidDate } from './isInvalidDate';
+
+describe('isInvalidDate', () => {
+  it.each([
+    { dateString: '2022-07-17', expected: false },
+    { dateString: 'invalid date', expected: true },
+  ])(
+    'should return $expected if $dateString are entered',
+    ({ dateString, expected }) => {
+      expect.hasAssertions();
+
+      const date = new Date(dateString);
+
+      expect(isInvalidDate(date)).toBe(expected);
+    },
+  );
+});
