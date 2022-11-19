be6be6be998f4098b5de5b870d4933e8eaea9ee9
t-i-0414 <gutlo.ol0414@gmail.com>
2022-11-19 11:23:18 +0900

    refactor: organisms storybook components

--- packages/takudev-web/components/organisms/ColorModeSwitchButton/ColorModeSwitchButton.stories.tsx
+++ packages/takudev-web/components/organisms/ColorModeSwitchButton/ColorModeSwitchButton.stories.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import { ColorModeSwitchButton } from '.';
-import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { ComponentStoryObj, ComponentMeta } from '@storybook/react';
 import {
   LightThemeTemplate,
   DarkThemeTemplate,
 } from '~/.storybook/ColorThemeTemplates';
+
+type StoryType = ComponentStoryObj<typeof ColorModeSwitchButton>;
 
 export default {
   title: 'Organisms/ColorModeSwitchButton',
   component: ColorModeSwitchButton,
 } as ComponentMeta<typeof ColorModeSwitchButton>;
 
-const Template: ComponentStory<typeof ColorModeSwitchButton> = () => (
-  <div style={{ backgroundColor: 'var(--header-color)' }}>
-    <ColorModeSwitchButton />
-  </div>
-);
+export const Light: StoryType = {
+  decorators: [
+    Story => (
+      <LightThemeTemplate>
+        <div style={{ backgroundColor: 'var(--header-color)' }}>
+          <Story />
+        </div>
+      </LightThemeTemplate>
+    ),
+  ],
+};
 
-export const Light = Template.bind({});
-Light.decorators = [
-  Story => (
-    <LightThemeTemplate>
-      <Story />
-    </LightThemeTemplate>
-  ),
-];
-
-export const Dark = Template.bind({});
-Dark.decorators = [
-  Story => (
-    <DarkThemeTemplate>
-      <Story />
-    </DarkThemeTemplate>
-  ),
-];
+export const Dark: StoryType = {
+  decorators: [
+    Story => (
+      <DarkThemeTemplate>
+        <div style={{ backgroundColor: 'var(--header-color)' }}>
+          <Story />
+        </div>
+      </DarkThemeTemplate>
+    ),
+  ],
+};
