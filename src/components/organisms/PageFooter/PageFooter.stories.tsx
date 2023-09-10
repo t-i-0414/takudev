ef8ad8c42e5cb02435980299ac9a7e5c1771ad66
t-i-0414 <gutlo.ol0414@gmail.com>
2023-09-10 17:14:06 +0900

    feat: 🎸 ページフッターにpanda cssを適用する

--- src/components/organisms/PageFooter/PageFooter.stories.tsx
+++ src/components/organisms/PageFooter/PageFooter.stories.tsx
@@ -2,11 +2,6 @@ import { PageFooter } from '.';
 
 import type { StoryObj, Meta } from '@storybook/react';
 
-import {
-  LightThemeTemplate,
-  DarkThemeTemplate,
-} from 'storybook/ColorThemeTemplates';
-
 type StoryType = StoryObj<typeof PageFooter>;
 
 export default {
@@ -14,22 +9,4 @@ export default {
   component: PageFooter,
 } as Meta<typeof PageFooter>;
 
-export const Light: StoryType = {
-  decorators: [
-    (Story, context) => (
-      <LightThemeTemplate>
-        <Story {...context.args} />
-      </LightThemeTemplate>
-    ),
-  ],
-};
-
-export const Dark: StoryType = {
-  decorators: [
-    (Story, context) => (
-      <DarkThemeTemplate>
-        <Story {...context.args} />
-      </DarkThemeTemplate>
-    ),
-  ],
-};
+export const Default: StoryType = {};
--- src/components/organisms/PageFooter/PageFooterView.tsx
+++ src/components/organisms/PageFooter/PageFooterView.tsx
@@ -1,35 +1,77 @@
 import React from 'react';
 
-import styles from './PageFooter.module.scss';
-
+import { css } from 'styled-system/css';
 import { Logo } from '~/components/atoms';
 import { SnsIconList } from '~/components/molecules';
 import { hrefMap } from '~/consts';
 
 export const PageFooterView: React.FC = () => (
-  <footer className={styles.footer}>
-    <div className={styles['logo-container']}>
+  <footer
+    className={css({
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      paddingTop: '16px',
+      backgroundColor: '#0D0D0D',
+    })}
+  >
+    <div className={css({ marginBottom: '16px' })}>
       <Logo href={hrefMap.home} />
     </div>
 
-    <ul className={styles['menu-list']}>
-      <li>
+    <ul
+      className={css({
+        padding: 0,
+        margin: '0 0 16px',
+      })}
+    >
+      <li className={css({ listStyle: 'none' })}>
         <a
           href={hrefMap.contact}
           aria-label='go to contact page'
           target='_blank'
           rel='noreferrer'
+          className={css({
+            padding: 0,
+            fontSize: '20px',
+            fontWeight: 'bold',
+            color: '#fff',
+            textDecoration: 'none',
+
+            _hover: {
+              opacity: 0.8,
+            },
+
+            _focusVisible: {
+              opacity: 0.8,
+            },
+
+            _active: {
+              opacity: 0.6,
+            },
+          })}
         >
           Contact
         </a>
       </li>
     </ul>
 
-    <div className={styles['sns-icon-list-container']}>
+    <div
+      className={css({
+        width: '328px',
+        marginBottom: '16px',
+      })}
+    >
       <SnsIconList colorName='white' />
     </div>
 
-    <small className={styles['copy-right']}>
+    <small
+      className={css({
+        marginBottom: '10px',
+        fontSize: '12px',
+        color: '#fff',
+      })}
+    >
       &copy;Takuya Iwashiro All Rights Reserved.
     </small>
   </footer>
